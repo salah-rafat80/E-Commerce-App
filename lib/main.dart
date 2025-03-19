f39039d0c5d78d35ab551623a7fdc2c82c222a9e
Salah Raafat <salahrafat2222@gmail.com>
2025-03-19 23:58:34 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-import 'features/Onboarding/splash_screen.dart';
+// import 'features/Onboarding/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
